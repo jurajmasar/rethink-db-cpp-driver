eba3787e0512b04f000ab7b33fea3b35078d0e4c
Juraj Masar <mail@jurajmasar.com>
2013-12-06 04:45:59 +0100

    It is now possible to create new db.

--- rethink_db.cpp
+++ rethink_db.cpp
@@ -11,32 +11,12 @@ namespace com {
 			this->is_connected = false;
 		}
 
-		int connection::connect() {
-			boost::asio::ip::tcp::resolver::query query(this->host, this->port);
-			resolver_.async_resolve(query,
-				boost::bind(&connection::handle_resolve, this,
-				boost::asio::placeholders::error,
-				boost::asio::placeholders::iterator));
+		void connection::connect() {
+			try {
+				boost::asio::ip::tcp::resolver::query query(this->host, this->port);
+				boost::asio::ip::tcp::resolver::iterator iterator = resolver_.resolve(query);
+				boost::asio::connect(socket_, iterator);
 
-			io_service.run();
-
-			return 0;
-		}
-
-		void connection::handle_resolve(const boost::system::error_code& err, boost::asio::ip::tcp::resolver::iterator endpoint_iterator) {
-			if (!err) {
-				// Attempt a connection to each endpoint in the list until we
-				// successfully establish a connection.
-				boost::asio::async_connect(socket_, endpoint_iterator,
-					boost::bind(&connection::handle_connect, this,
-					boost::asio::placeholders::error));
-			} else {
-				std::cout << "Error in handle_resolve: " << err.message() << "\n";
-			}
-		}
-
-		void connection::handle_connect(const boost::system::error_code& err) {
-			if (!err) {
 				std::ostream request_stream(&request_);
 
 				// send magic version number
@@ -49,71 +29,65 @@ namespace com {
 				// send auth_key
 				request_stream.write(auth_key.c_str(), auth_key.length());
 
-				// The connection was successful. Send the request.
-				boost::asio::async_write(socket_, request_,
-					boost::bind(&connection::handle_write_request, this,
-					boost::asio::placeholders::error));
-			} else {
-				std::cout << "Error in handle_connect: " << err.message() << "\n";
-			}
-		}
-		
-		void connection::handle_write_request(const boost::system::error_code& err) {
-			std::cout << "In handle_write_request" << std::endl;
-			if (!err) {
-				// Read the response status line. The response_ streambuf will
-				// automatically grow to accommodate the entire line. The growth may be
-				// limited by passing a maximum size to the streambuf constructor.
-				boost::asio::async_read_until(socket_, response_, 0,
-					boost::bind(&connection::handle_read_connect_response, this,
-					boost::asio::placeholders::error));
-			} else {
-				std::cout << "Error in handle_write_request: " << err.message() << "\n";
-			}
-		}
+				boost::asio::write(socket_, request_);
 
-		void connection::handle_read_connect_response(const boost::system::error_code& err) {
-			if (!err) {
+				boost::asio::read_until(socket_, response_, 0);
+
 				std::istream response_stream(&response_);
 				std::string response;
 				std::getline(response_stream, response);
-				if (response.substr(0,7) == "SUCCESS") {
+				if (response.substr(0, 7) == "SUCCESS") {
 					std::cout << "Successfully connected." << std::endl;
 					std::cout << "Response: '" << response << "'\n";
 					this->is_connected = true;
-				} else {
+				}
+				else {
 					this->is_connected = false;
 					std::cout << "Error in handle_read_connect_response: '" << response << "'\n";
-				}
-			} else {
-				std::cout << "Error in handle_read_connect_response: " << err << "\n";
+				}			
 			}
+			catch (std::exception& e)
+			{
+				std::cerr << "Exception: " << e.what() << "\n";
+			}			
 		}
+
+		
 		
 		void connection::create_db(std::string db_name) {
-			com::rethinkdb::Term t = com::rethinkdb::Term();
-			t.set_type(com::rethinkdb::Term::TermType::Term_TermType_DB_CREATE);
+			com::rethinkdb::Query q = com::rethinkdb::Query();
+			q.set_type(com::rethinkdb::Query::QueryType::Query_QueryType_START);
+			q.set_token(1);
+
+			com::rethinkdb::Term *t;
+			t = q.mutable_query();
+			t->set_type(com::rethinkdb::Term::TermType::Term_TermType_DB_CREATE);
 
 			com::rethinkdb::Term *t_name;
-			t_name = t.add_args();
+			t_name = t->add_args();
 			t_name->set_type(com::rethinkdb::Term::TermType::Term_TermType_DATUM);
-			std::cout << "Debug point." << std::endl;
+
 			com::rethinkdb::Datum *datum;
 			datum = t_name->mutable_datum();
 			datum->set_type(com::rethinkdb::Datum::DatumType::Datum_DatumType_R_STR);
-			datum->set_r_str(db_name);
-			
+			datum->set_r_str(db_name);			
+
 			std::ostream request_stream(&request_);
-			std::string blob = t.SerializeAsString();
+			std::string blob = q.SerializeAsString();
 			
 			u_int blob_length = blob.length();
-			request_stream.write((char*)&blob, sizeof (u_int));
+			request_stream.write((char*)&blob_length, sizeof (u_int));
 
 			request_stream.write(blob.c_str(), blob.length());
+			try {
+				boost::asio::write(socket_, request_);
 
-			boost::asio::async_write(socket_, request_,
-				boost::bind(&connection::handle_write_request, this,
-				boost::asio::placeholders::error));
+			}
+			catch (std::exception& e)
+			{
+				std::cerr << "Exception: " << e.what() << "\n";
+			}
+
 		} 
 	}
 }
--- rethink_db.hpp
+++ rethink_db.hpp
@@ -4,10 +4,7 @@
 #include <stdint.h>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
-#include <boost/function.hpp>
-#include <boost/bind/protect.hpp>
 #include <stdlib.h>
-#include <boost/algorithm/string.hpp>
 
 // fix for undefined ssize_t from https://code.google.com/p/cpp-btree/issues/detail?id=6
 #if defined(_MSC_VER)
@@ -25,7 +22,7 @@ namespace com {
 			public:
 
 				connection(const std::string& host, const std::string& port, const std::string& database, const std::string& auth_key, int timeout);
-				int connect();
+				void connect();
 				void create_db(std::string db_name);
 			private:
 
@@ -43,12 +40,6 @@ namespace com {
 				boost::asio::streambuf request_;
 				boost::asio::streambuf response_;
 
-				void handle_resolve(const boost::system::error_code& err, boost::asio::ip::tcp::resolver::iterator endpoint_iterator);
-				void handle_connect(const boost::system::error_code& err);
-
-				void handle_write_request(const boost::system::error_code& err);
-
-				void handle_read_connect_response(const boost::system::error_code& err);
 
 		};
 	}
